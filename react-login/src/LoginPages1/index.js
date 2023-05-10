b89df6cd2711d032d9588790a539595a4f972f0f
Frantisek Pelech <feromiro23@gmail.com>
2023-05-11 01:20:09 +0200

    First login react page preparation

---
+++ react-login/src/LoginPages1/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+function LoginPage1() {
+  return (
+    <div>
+      <button className="btn btn-primary">Bootstrap button</button>
+    </div>
+  );
+}
+
+export default LoginPage1;
