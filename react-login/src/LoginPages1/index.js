31425ee115005e7db8663edb0ea3c4ee63c7330b
Frantisek Pelech <feromiro23@gmail.com>
2023-05-11 15:39:56 +0200

    First settings on login-page-1

--- react-login/src/LoginPages1/index.js
+++ react-login/src/LoginPages1/index.js
@@ -2,8 +2,22 @@ import React from "react";
 import "./index.css";
 function LoginPage1() {
   return (
-    <div>
-      <button className="btn btn-primary">Bootstrap button</button>
+    <div className="login-page-1">
+      <div className="row">
+        <div className="col-md-6 d-flex align-items-center justify-content-center">
+          <div className="text-part d-flex flex-column">
+            <h1>LOGIN</h1>
+            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Password" />
+            <button>LOGIN</button>
+            <div className="d-flex justify-content-between">
+              <a href="">Forgot Password?</a>
+              <a href="">Register</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6"></div>
+      </div>
     </div>
   );
 }
