608a60e898d5f64688297b1a8609bfcf056f26c8
Frantisek Pelech <feromiro23@gmail.com>
2023-05-18 01:53:59 +0200

    Final version

--- react-login/src/LoginPages1/index.js
+++ react-login/src/LoginPages1/index.js
@@ -16,8 +16,14 @@ function LoginPage1() {
             </div>
           </div>
         </div>
-        <div className="col-md-6"></div>
+        <div className="col-md-6 d-flex align-items-center">
+          <img
+            src="https://media.istockphoto.com/id/1154370446/photo/funny-raccoon-in-green-sunglasses-showing-a-rock-gesture-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=kkZiaB9Q-GbY5gjf6WWURzEpLzNrpjZp_tn09GB21bI="
+            alt="FunkyBear"
+          />
+        </div>
       </div>
+      <div className="login-page-1-rectangle"></div>
     </div>
   );
 }
